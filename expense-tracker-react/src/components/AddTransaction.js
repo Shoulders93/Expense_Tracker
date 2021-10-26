d4b541050e71dc83702191bb4a091676740475a0
Kory Attleson <kory.attleson@gmail.com>
2021-10-26 09:46:12 -0500

    Updated GlobalState to add + delete transaction + updated AppReducer with Delete and Add cases + changed IncomeExpenses to input data instead of hard coded transactions + updated Balance to Add/Subtract amounts. End product of Front End React Expense Tracker Application.

--- expense-tracker-react/src/components/AddTransaction.js
+++ expense-tracker-react/src/components/AddTransaction.js
@@ -1,23 +1,40 @@
-import React, {useState} from 'react'
+import React, {useState, useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
 
 export const AddTransaction = () => {
     const [text, setText] = useState('');
     const [amount, setAmount] = useState(0);
+
+    const { addTransaction } = useContext(GlobalContext);
+
+    const onSubmit = event => {
+        event.preventDefault();
+
+        const newTransaction = {
+            id: Math.floor(Math.random() * 1000000),
+            text,
+            amount: +amount
+        }
+
+        addTransaction(newTransaction);
+    }
+
     return (
         <>
             <h3>Add new transaction</h3>
-            <form>
+            <form onSubmit={onSubmit}>
                 <div className='form-control'>
                     <label htmlFor='text'>Text</label>
                     <input type='text' value={text} onChange={(event) => setText(event.target.value)} placeholder='Enter text here..' />
                 </div>
                 <div>
-                    <label htmlfor='amount'
+                    <label htmlFor='amount'
                     >Amount <br />
                     (negative - expese, positive - income) </label
                     >
                     <input type='number' value={amount} onChange={(event) => setAmount(event.target.value)} placeholder='Enter amount...' />
                 </div>
+                <button className='btn'>Add Transaction</button>
             </form>
         </>
     )
